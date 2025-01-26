3133c78dc3e4fda53cee3ef1ad61f74be656bae3
Ahnaf19 <ahnaftanjid19@gmail.com>
2025-01-26 17:53:53 +0600

    project init

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+# from app.routers.student_router import router as student_router
+# from app.exceptions.exceptions import StudentNotFoundException, DuplicateStudentException
+# from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+# Include routers
+# app.include_router(student_router)
