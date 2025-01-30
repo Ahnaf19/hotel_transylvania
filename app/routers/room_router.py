a8e71ac5738e85264b2de78c619aa4fafb552272
Ahnaf19 <ahnaftanjid19@gmail.com>
2025-01-30 12:09:51 +0600

    room all v1

--- app/routers/room_router.py
+++ app/routers/room_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.data.room_data import dummy_room_data
+from app.data.room_data import RoomData, dummy_room_data
 from app.services.room_service import RoomService
 from app.schemas.room_schema import *
 
@@ -10,23 +10,22 @@ router = APIRouter(prefix="/rooms", tags=["rooms"])
 room_service = RoomService(dummy_room_data)
 
 # define the routes
-
 @router.get("/")
-async def read_rooms():
+async def read_rooms() -> RoomData:
     return room_service.dummy_room_data
 
 @router.get("/{room_id}", response_model=RoomBase)
-async def read_room(room_id: int):
+async def read_room(room_id: int) -> RoomBase:
     return room_service.get_room_by_id(room_id)
 
 @router.post("/add-room", response_model=RoomBase)
-async def add_room(room: RoomBase):
-    return room_service.add_room(room.model_dump())
+async def add_room(room: RoomBase) -> RoomBase:
+    return room_service.add_room(room)
 
 @router.delete("/{room_id}", response_model=RoomBase, responses={200: {"description": "Room deleted"}, 404: {"description": "Room not found"}})
-async def remove_room(room_id: int):
+async def remove_room(room_id: int) -> RoomBase:
     return room_service.delete_room_by_id(room_id)
 
 @router.put("/{room_id}")
-async def modify_room(room_id: int, updated_room: UpdateRoom):
+async def modify_room(room_id: int, updated_room: UpdateRoom) -> RoomBase:
     return room_service.update_room_by_id(room_id, updated_room)
--- app/services/room_service.py
+++ app/services/room_service.py
@@ -8,7 +8,7 @@ class RoomService:
         self.dummy_room_data = dummy_room_data
         logger.info("RoomService initialized")
     
-    def get_room_by_id(self, room_id: int):
+    def get_room_by_id(self, room_id: int) -> RoomBase:
         response_room = self.dummy_room_data.rooms.get(room_id, None)
         if response_room is None:
             logger.error(f"Room with id {room_id} not found")
@@ -16,8 +16,8 @@ class RoomService:
         logger.info(f"Room with id {room_id} found: {response_room.model_dump()}")
         return response_room
 
-    def add_room(self, room: RoomBase):
-        logger.info(f"Received room: {room.model_dump()}")
+    def add_room(self, room: RoomBase) -> RoomBase:
+        logger.info(f"Received room: {room}")
         if self.dummy_room_data.rooms.get(room.room_id, None):
             logger.error(f"Room with id {room.room_id} already exists")
             raise RoomAlreadyExistsException(room.room_id)
@@ -25,7 +25,7 @@ class RoomService:
         logger.info(f"Room with id {room.room_id} added: {room.model_dump()}")
         return room 
 
-    def delete_room_by_id(self, room_id: int):
+    def delete_room_by_id(self, room_id: int) -> RoomBase:
         deleted_room = self.dummy_room_data.rooms.pop(room_id, None)
         if deleted_room is None:
             logger.error(f"Room with id {room_id} not found")
@@ -33,7 +33,7 @@ class RoomService:
         logger.info(f"Room with id {room_id} deleted: {deleted_room.model_dump()}")
         return deleted_room
 
-    def update_room_by_id(self, room_id: int, update_room: UpdateRoom):
+    def update_room_by_id(self, room_id: int, update_room: UpdateRoom) -> RoomBase:
         response_room = self.get_room_by_id(room_id)
         update_data = update_room.model_dump(exclude_unset=True) # exclude_unset will exclude the data that is not set/updated
         response_room.update(**update_data) # update the data
